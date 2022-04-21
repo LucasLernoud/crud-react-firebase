8f6bc6f659b2f9d3985776881168b41c3d1ae1c5
Lucas Lernoud <lucaslerdb@gmail.com>
2022-04-21 15:51:03 +0200

    partie auth terminée reste à coder le CRUD sur firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React from "react";
+import { useState } from "react";
+import { useRef } from "react";
+import { auth } from "../utils/firebase.config";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>se connecter</h1>
-        </div>
-    );
+  const loginEmail = useRef();
+  const loginPassword = useRef();
+  const [error, setError] = useState(false)
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail.current.value,
+        loginPassword.current.value
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+      setError(true);
+    }
+
+    console.log(loginEmail.current.value, loginPassword.current.value);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login">
+        <h3>Se connecter</h3>
+        <form className="form-login" onSubmit={(e) => handleLogin(e)}>
+          <input type="email" placeholder="Email" required ref={loginEmail} />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            required
+            ref={loginPassword}
+          />
+          <input type="submit" value="Se connecter" />
+          <span>{error && "Le mail ou le mot de passe de correspondent pas"} </span>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
